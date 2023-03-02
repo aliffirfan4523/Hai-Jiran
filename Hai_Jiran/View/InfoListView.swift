261b7a1360ade1eede39ca56acbf0f373f716d50
Aliff Irfan <aliffirfanisk@gmail.com>
2023-03-02 11:18:12 +0800

    fix bug error

--- Hai_Jiran/View/InfoListView.swift
+++ Hai_Jiran/View/InfoListView.swift
@@ -44,24 +44,6 @@ struct MyInfoListView: View {
             }.listStyle(PlainListStyle())
             VStack {
             Spacer()
-            HStack {
-            Spacer()
-            Button(action: {
-            }, label: {
-                Text("+")
-                    .font(.system(.largeTitle))
-                    .frame(width: 45, height: 40)
-                    .foregroundColor(Color.white)
-                    .padding(.bottom, 4)
-                })
-                .background(Color(0x5bd4a4))
-                .cornerRadius(50)
-                .padding(EdgeInsets(top: 0, leading: 0, bottom: 20, trailing: 30))
-                .shadow(color: Color.black.opacity(0.3),
-                    radius: 3,
-                    x: 3,
-                    y: 3)
-                }
             }
         }
     }
